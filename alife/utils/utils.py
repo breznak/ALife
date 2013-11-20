bd3134b43e14c53ca577f07fed99fc0ca79d4a8d
Marek Otahal <markotahal@gmail.com>
2013-11-20 15:07:19 +0100

    add zeros = init neutral function for dict3d

--- alife/utils/utils.py
+++ alife/utils/utils.py
@@ -7,3 +7,15 @@ def dumpToArray(val, dict3d):
     for y in dict3d[x]:
       ar[x][y]=dict3d[x][y][val]
   return ar
+
+def zeros(listKeys, dict3d, zero=0):
+  """clears; filles a 3D dictionary;
+     params:
+       listKeys -- list of keys for inner-most dict ("score","visited",...)
+       dict3d -- 3D map, dict, matrix
+       zero = the filler value, (default 0)"""
+  for x in dict3d:
+    for y in dict3d[x]:
+      for key in listKeys:
+        dict3d[x][y][key]=zero
+  return dict3d
