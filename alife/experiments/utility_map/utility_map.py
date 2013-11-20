3512994239ff16947b1a05bd37ee8d155fad9a3c
Marek Otahal <markotahal@gmail.com>
2013-11-20 15:59:07 +0100

    UtilityMap experiment: add commandline params

--- alife/experiments/utility_map/utility_map.py
+++ alife/experiments/utility_map/utility_map.py
@@ -4,32 +4,32 @@ from alife.worlds.world import World, Point
 from alife.agents.UtilityAgent import SimpleAgent
 from alife.utils.utils import dumpToArray
 import math
+import sys
 from nupic.encoders.utility import SimpleUtilityEncoder
 
 # common settings:
-dimX=25
-dimY=25
 items=None
 target=Point(4,9)
 
-
-def main():
-  w = World(dimX, dimY, items)
+def main(targetX=4, targetY=9, dimX=25, dimY=25):
+  global target
+  target=Point(int(targetX), int(targetY))
+  w = World(int(dimX), int(dimY), items)
   ag = SimpleAgent()
   ag.world=w
   ag.actions['go']=go
   ag.targets=[reachedTarget]
-  ag.util = SimpleUtilityEncoder(length=2, minval=0, maxval=max(dimX,dimY)-1, scoreMin=0, scoreMax=100, scoreResolution=0.1)
+  ag.util = SimpleUtilityEncoder(length=2, minval=0, maxval=max(int(dimX),int(dimY)), scoreMin=0, scoreMax=100, scoreResolution=0.1)
   ag.util.setEvaluationFn(euclDistance)
   ag.start=ag.world._getRandomPos()
 
   # walk it, baby
-  for x in xrange(dimX):
-    for y in xrange(dimY):
+  for x in xrange(w.dimX):
+    for y in xrange(w.dimY):
       ag.actions['go'](ag, x, y)
 
   utilityMap = dumpToArray('score', ag.mem)
-  print utilityMap
+#  print utilityMap
 
   _visualize(utilityMap)
 
@@ -43,6 +43,7 @@ def reachedTarget(ag, target):
 # defines score:
 def euclDistance(listCoords):
   """eval fn for agent is distance to target """
+  global target
   x=listCoords[0]
   y=float(listCoords[1])
   tx=target.x
@@ -80,4 +81,10 @@ def _visualize(map):
 
 #########################################
 if __name__ == "__main__":
+  if(len(sys.argv)==3):
+    main(sys.argv[1], sys.argv[2])
+  elif(len(sys.argv)==5):
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+  else:
+    print "Use: \n python /path/to/utility_map.py [targetX targetY [dimX dimY]]"
     main()
